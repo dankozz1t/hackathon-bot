58edfe304ca544dafed29c62272bfee07470e8d5
Alex-Taylor Danko <AlexDankoXXL@gmail.com>
2023-12-09 11:31:41 +0200

    update information

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+const pilgi = require("./pіlgi");
+
 const { Bot, Keyboard, InputFile, session } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
@@ -6,21 +9,43 @@ const bot = new Bot(process.env.BOT_API_KEY);
 function initial() {
   return { questionIndex: undefined, score: undefined };
 }
+
+let questions = [
+  "1. Думки та спогади, що повторюються і турбують, або нав’язливі картини стресового досвіду з минулого?",
+  "2. Повторювані, хвилюючі сновидіння про стресовий досвід?",
+  "3. Раптове відчуття ніби стресовий досвід знову трапляється (переживаєте ситуацію знову)?",
+  "4. Відчуття пригнічення, смутку, коли щось нагадувало вам стресову ситуацію з минулого?",
+  "5. Сильні фізичні реакції, коли щось нагадувало про стресовий досвід (наприклад, серцебиття, утруднене дихання,",
+  "6. Уникання думок або розмов про стресову ситуацію у минулому або уникання почуттів, пов’язаних з цією ситуацією?",
+  "7. Уникання певної діяльності або ситуацій, тому що вони нагадують вам стресову ситуацію з минулого?",
+  "8. Труднощі з пригадуванням важливих моментів стресового досвіду з минулого?",
+  "9. Сильні негативні переконання про себе, інших людей або навколишній світ (наприклад, «я поганий», «зі мною щось дуже не так», «нікому не можна довіряти», «світ – небезпечне місце»)?",
+  "10. Звинувачення себе або інших на рахунок стресового досвіду, або того, що сталося після нього?",
+  "11. Сильні негативні емоції, такі як страх, жах, злість, почуття провини або сором?",
+  "13. Відчуття віддаленості або відокремленості від інших?",
+  "14. Проблеми у переживанні позитивних емоцій (наприклад, незмога відчувати радість або любов до близької людини)?",
+  "15. Роздратування, спалахи гніву, агресивна поведінка?",
+  "16. Ризикова поведінка, яка може зашкодити?",
+  "17. Перебування «на взводі» або «на сторожі»?",
+  "18. Відчуття постійної напруги?",
+  "19. Труднощі із зосередженістю?",
+  "20. Проблеми із засинанням або нічні прокидання?",
+];
+
 bot.use(session({ initial }));
 
 function startWellnessTest(ctx) {
-  const questions = ["Питання 1", "Питання 2"];
-
   const keyboard = new Keyboard()
     .text("Зовсім ні – 0")
-    .row()
     .text("Трохи – 1")
     .row()
     .text("Помірно – 2")
-    .row()
     .text("Відчутно – 3")
     .row()
-    .text("Дуже – 4");
+    .text("Дуже – 4")
+    .row()
+    .text("Повернутись до головного меню")
+    .text("Допомога спеціаліста");
 
   ctx.reply(`${questions[0]}`, {
     reply_markup: { keyboard: keyboard.build(), resize_keyboard: true },
@@ -30,23 +55,24 @@ function startWellnessTest(ctx) {
 }
 
 const mainMenu = new Keyboard()
-  .text("Інформація про пільги")
+  .text("Гарантії та пільги")
   .row()
-  .text("Пройти тест на самопочуття")
+  .text("Пройти тест на посттравматичний стресовий розлад (ПТСР)")
   .row()
   .text("Термінова допомога спеціаліста");
 
 bot.command("start", async (ctx) => {
   console.log(`use start:`, ctx.message.from);
 
-  const localImagePath = "images/first.jpg";
+  const localImagePath = "images/header.jpg";
   await ctx.replyWithPhoto(new InputFile(localImagePath), {
-    caption: "Вітаю! Оберіть опцію:",
+    caption: `Привіт! Я твій вірний цифровий помічник у світі ветеранів. 
+Обери те, що тебе цікавить у меню: `,
     reply_markup: { keyboard: mainMenu.build(), resize_keyboard: true },
   });
 });
 
-bot.on("message:text", (ctx) => {
+bot.on("message:text", async (ctx) => {
   const text = ctx.message.text;
   console.log(`use text:`, ctx.message.from);
   console.log(`text:`, text);
@@ -54,11 +80,11 @@ bot.on("message:text", (ctx) => {
   const themeMenu = {
     reply_markup: {
       keyboard: new Keyboard()
-        .text("Тема1")
-        .text("Тема2")
+        .text("Медичні пільги")
+        .text("Реабілітація")
         .row()
-        .text("Тема3")
-        .text("Тема4")
+        .text("Освітні пільги")
+        .text("Забезпечення автомобілем та місцем для нього")
         .row()
         .text("Повернутись до головного меню")
         .build(),
@@ -67,29 +93,49 @@ bot.on("message:text", (ctx) => {
     },
   };
 
-  if (text === "Інформація про пільги") {
+  if (text === "Гарантії та пільги") {
     ctx.reply("Оберіть тему:", themeMenu);
-  } else if (text === "Тема1") {
-    ctx.reply("Інформація про Тему1", themeMenu);
-  } else if (text === "Тема2") {
-    ctx.reply("Інформація про Тему2", themeMenu);
-  } else if (text === "Тема3") {
-    ctx.reply("Інформація про Тему3", themeMenu);
-  } else if (text === "Тема4") {
-    ctx.reply("Інформація про Тема4", themeMenu);
-  } else if (text === "Пройти тест на самопочуття") {
+  } else if (text === "Медичні пільги") {
+    ctx.reply(pilgi.pilgi1, { parse_mode: "HTML" });
+  } else if (text === "Реабілітація") {
+    ctx.reply(pilgi.pilgi2, { parse_mode: "HTML" });
+  } else if (text === "Освітні пільги") {
+    ctx.reply(pilgi.pilgi3, { parse_mode: "HTML" });
+  } else if (text === "Забезпечення автомобілем та місцем для нього") {
+    ctx.reply(pilgi.pilgi4, { parse_mode: "HTML" });
+  } else if (
+    text === "Пройти тест на посттравматичний стресовий розлад (ПТСР)"
+  ) {
+    await ctx.reply(
+      `<b>Шкала самооцінки проявів посттравматичного стресового розладу (ПТСР). Методика PCL-5</b>
+      
+Нижче вказані реакції, які іноді бувають після пережитого стресу. Уважно прочитайте кожен пункт та виберіть відповідь, яка відображає, наскільки сильно вас турбувала зазначена проблема протягом останнього місяця.
+
+Всього буде <b>20</b> питань 
+      `,
+      { parse_mode: "HTML" }
+    );
+
     startWellnessTest(ctx);
-  } else if (text === "Термінова допомога спеціаліста") {
+  } else if (
+    text === "Термінова допомога спеціаліста" ||
+    text === "Допомога спеціаліста"
+  ) {
     ctx.reply("Надішліть, будь ласка, свій контактний номер:", {
       reply_markup: {
         keyboard: new Keyboard().requestContact("Надіслати контакт").build(),
         resize_keyboard: true,
       },
     });
+  } else if (text === "Повернутись до головного меню") {
+    ctx.reply("Обери те, що тебе цікавить у меню: ", {
+      reply_markup: {
+        keyboard: mainMenu.build(),
+        resize_keyboard: true,
+      },
+    });
   } else if (ctx.session?.questionIndex !== undefined) {
-    console.log(`ctx.session?.questionIndex:`, ctx.session?.questionIndex);
     const answerScore = parseInt(text.split(" – ")[1]);
-    const questions = ["Питання 1", "Питання 2"];
 
     ctx.session.score += answerScore;
     if (ctx.session?.questionIndex < questions.length - 1) {
@@ -98,51 +144,67 @@ bot.on("message:text", (ctx) => {
         reply_markup: {
           keyboard: new Keyboard()
             .text("Зовсім ні – 0")
-            .row()
             .text("Трохи – 1")
             .row()
             .text("Помірно – 2")
-            .row()
             .text("Відчутно – 3")
             .row()
             .text("Дуже – 4")
+            .row()
+            .text("Повернутись до головного меню")
+            .text("Допомога спеціаліста")
             .build(),
           resize_keyboard: true,
         },
       });
     } else {
-      ctx.reply(`Ваш результат: ${ctx.session.score}`, {
-        reply_markup: {
-          keyboard: new Keyboard()
-            .text("Повернутись до головного меню")
-            .build(),
-          resize_keyboard: true,
-        },
-      });
+      ctx.reply(
+        `Ваш результат:  ${ctx.session.score} балів
+      
+      
+За Вашими результатами ${
+          ctx.session.score < 35
+            ? "посттравматичний розлад малоймовірний."
+            : "є імовірність присутності посттравматичного розладу."
+        }
+      
+Важливо розуміти, що цей тест є доволі корисним у виявленні симптомів ПТСР, однак він не дає можливості діагностувати його. Це може зробити винятково спеціаліст з психічного здоров’я. 
+
+Якщо вас турбують окремі прояви з зазначених вище у сильній формі, рекомендуємо вам звернутися до психолога психотерапевти або ж лікаря психотерапевта.
+      `,
+        {
+          reply_markup: {
+            keyboard: new Keyboard()
+              .text("Повернутись до головного меню")
+              .text("Допомога спеціаліста")
+              .build(),
+            resize_keyboard: true,
+          },
+        }
+      );
       delete ctx.session.questionIndex;
       delete ctx.session.score;
     }
-  } else if (text === "Повернутись до головного меню") {
-    ctx.reply("Вітаю! Оберіть опцію:", {
-      reply_markup: {
-        keyboard: mainMenu.build(),
-        resize_keyboard: true,
-      },
-    });
   } else {
-    ctx.reply("Не розумію, виберіть щось з меню", {
-      reply_markup: {
-        keyboard: mainMenu.build(),
-        resize_keyboard: true,
-      },
-    });
+    ctx.reply(
+      "Вибачте, але я не розумію що ви маєте на увазі, виберіть щось з меню",
+      {
+        reply_markup: {
+          keyboard: mainMenu.build(),
+          resize_keyboard: true,
+        },
+      }
+    );
   }
 });
 
 bot.on("message:contact", (ctx) => {
-  return ctx.reply("Дякую за надсилання вашого контакту!", {
-    reply_markup: { keyboard: mainMenu.build(), resize_keyboard: true },
-  });
+  return ctx.reply(
+    "Дякую за надсилання вашого контакту! Наші фахівці зв'яжуться з вами якнайшвидше",
+    {
+      reply_markup: { keyboard: mainMenu.build(), resize_keyboard: true },
+    }
+  );
 });
 
 bot.start();
---
+++ pіlgi.js
@@ -0,0 +1,116 @@
+module.exports = {
+  pilgi1: `
+<b>Першочергові безоплатні ліки за рецептами лікарів</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergovi-bezoplatni-liki-za-receptami-likariv">Посилання</a>
+
+<b>Доставка за висновком лікаря ліків додому</b>
+<a href="https://eveteran.gov.ua/benefits/dostavka-za-visnovkom-likarya-likiv-dodomu">Посилання</a>
+
+<b>Безоплатні ліки за рецептами лікарів</b>
+<a href="https://eveteran.gov.ua/benefits/zabezpechennya-bezoplatnimi-likami">Посилання</a>
+
+<b>Безоплатне першочергове зубопротезування (за винятком протезування з дорогоцінних металів)</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-pershochergove-zuboprotezuvannya-za-vinyatkom-protezuvannya-z-dorogocinnih-metaliv">Посилання</a>
+
+<b>Безоплатне обслуговування у закладах охорони здоров'я, до яких вони були прикріплені за попереднім місцем роботи, при виході на пенсію</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-obslugovuvannya-u-zakladah-ohoroni-zdorovya-do-yakih-voni-buli-prikripleni-za-poperednim-miscem-roboti-pri-vihodi-na-pensiyu">Посилання</a>
+
+<b>Безоплатне обслуговування у закладах охорони здоров'я, до яких вони були прикріплені за попереднім місцем роботи, після зміни місця роботи</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-obslugovuvannya-u-zakladah-ohoroni-zdorovya-do-yakih-voni-buli-prikripleni-za-poperednim-miscem-roboti-pislya-zmini-miscya-roboti">Посилання</a>
+
+<b>Щорічне медичне обстеження із залученням необхідних спеціалістів</b>
+<a href="https://eveteran.gov.ua/benefits/shchorichne-medichne-obstezhennya-iz-zaluchennyam-neobhidnih-specialistiv">Посилання</a>
+
+<b>Право на щорічне медичне обстеження із залученням необхідних спеціалістів</b>
+<a href="https://eveteran.gov.ua/benefits/pravo-na-shchorichne-medichne-obstezhennya-iz-zaluchennyam-neobhidnih-specialistiv">Посилання</a>
+
+<b>Позачергове безплатне лікування у стаціонарах, госпіталях, лікарнях</b>
+<a href="https://eveteran.gov.ua/benefits/pravo-na-shchorichne-medichne-obstezhennya-iz-zaluchennyam-neobhidnih-specialistiv">Посилання</a>
+
+<b>Першочергове обслуговування в лікувально-профілактичних закладах</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergove-obslugovuvannya-v-likuvalno-profilaktichnih-zakladah">Посилання</a>
+`,
+
+  pilgi2: `
+<b>Безоплатне забезпечення санаторно-курортним лікуванням</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-zabezpechennya-sanatorno-kurortnim-likuvannyam">Посилання</a>
+
+<b>Безоплатне першочергове забезпечення санаторно-курортним лікуванням</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-pershochergove-zabezpechennya-sanatorno-kurortnim-likuvannyam">Посилання</a>
+
+<b>Безоплатне забезпечення санаторно-курортним лікуванням першочергово із числа позачерговиків</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-zabezpechennya-sanatorno-kurortnim-likuvannyam-pershochergovo-iz-chisla-pozachergovikiv">Посилання</a>
+
+<b>Грошова компенсація вартості путівки</b>
+<a href="https://eveteran.gov.ua/benefits/groshova-kompensaciya-vartosti-putivki">Посилання</a>
+
+<b>Компенсація вартості самостійного санаторно-курортного лікування</b>
+<a href="https://eveteran.gov.ua/benefits/kompensaciya-vartosti-samostijnogo-sanatorno-kurortnogo-likuvannya">Посилання</a>
+
+<b>Компенсація вартості проїзду до санаторно-курортного закладу і назад</b>
+<a href="https://eveteran.gov.ua/benefits/kompensaciya-vartosti-proyizdu-do-sanatorno-kurortnogo-zakladu-i-nazad">Посилання</a>
+
+<b>Безоплатне забезпечення іншими протезами і протезно-ортопедичними виробами</b>
+<a href="https://eveteran.gov.ua/benefits/bezplatne-zabezpechennya-inshimi-protezami-i-protezno-ortopedichnimi-virobami">Посилання</a>
+`,
+
+  pilgi3: `
+<b>Надання переважного права на вступ до закладів вищої, фахової передвищої освіти</b>
+<a href="https://eveteran.gov.ua/benefits/nadannya-perevazhnogo-prava-na-vstup-do-zakladiv-vishchoyi-fahovoyi-peredvishchoyi-osviti">Посилання</a>
+
+<b>Надання права на позаконкурсний вступ до закладів професійної (професійно-технічної) освіти</b>
+<a href="https://eveteran.gov.ua/benefits/nadannya-prava-na-pozakonkursnij-vstup-do-zakladiv-profesijnoyi-profesijno-tehnichnoyi-osviti">Посилання</a>
+
+<b>Повна або часткова оплата навчання за рахунок коштів державного та місцевих бюджетів</b>
+<a href="https://eveteran.gov.ua/benefits/derzhavna-cilova-pidtrimka">Посилання</a>
+
+<b>Пільговий довгостроковий кредит для здобуття освіти</b>
+<a href="https://eveteran.gov.ua/benefits/pilgovij-dovgostrokovij-kredit-dlya-zdobuttya-osviti">Посилання</a>
+
+<b>Соціальна стипендія</b>
+<a href="https://eveteran.gov.ua/benefits/socialna-stipendiya">Посилання</a>
+
+<b>Першочергове переведення здобувачів освіти на навчання на місцях державного (регіонального) замовлення</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergove-perevedennya-zdobuvachiv-osviti-na-navchannya-na-miscyah-derzhavnogo-regionalnogo-zamovlennya">Посилання</a>
+
+<b>Безоплатне проживання в гуртожитку</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-prozhivannya-v-gurtozhitku">Посилання</a>
+
+<b>Безоплатний доступ до мережі Інтернет, систем баз даних в закладах освіти</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatnij-dostup-do-merezhi-internet-sistem-baz-danih-v-zakladah-osviti">Посилання</a>
+
+<b>Безоплатне забезпечення підручниками</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-zabezpechennya-pidruchnikami">Посилання</a>
+`,
+  pilgi4: `
+<b>Першочерговість вступу до кооперативів по будівництву та експлуатації колективних гаражів</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergovist-vstupu-do-kooperativiv-po-budivnictvu-ta-ekspluataciyi-kolektivnih-garazhiv">Посилання</a>
+
+<b>Звільнення від орендної плати за нежилі приміщення, що орендуються особами з інвалідністю внаслідок війни під гаражі для спеціальних засобів пересування (автомобілів, мотоколясок, велоколясок тощо)</b>
+<a href="https://eveteran.gov.ua/benefits/zvilnennya-vid-orendnoyi-plati-za-nezhili-primishchennya-shcho-orenduyutsya-osobami-z-invalidnistyu-vnaslidok-vijni-pid-garazhi-dlya-specialnih-zasobiv-peresuvannya-avtomobiliv-motokolyasok-velokolyasok-toshcho">Посилання</a>
+
+<b>Безоплатне надання для спеціальних засобів пересування (автомобілів, мотоколясок, велоколясок тощо) гаражів-стоянок незалежно від їх форми власності.</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-nadannya-dlya-specialnih-zasobiv-peresuvannya-avtomobiliv-motokolyasok-velokolyasok-toshcho-garazhiv-stoyanok-nezalezhno-vid-yih-formi-vlasnosti">Посилання</a>
+
+<b>Першочергове право на вступ до кооперативів по будівництву</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergove-pravo-na-vstup-do-kooperativiv-po-budivnictvu-ta-ekspluataciyi-stoyanok-dlya-tz">Посилання</a>
+
+<b>Позачергове безоплатне забезпечення автомобілем</b>
+<a href="https://eveteran.gov.ua/benefits/pozachergove-bezoplatne-zabezpechennya-avtomobilem">Посилання</a>
+
+<b>Позачергове безоплатне забезпечення автомобілем - заміна на новий</b>
+<a href="https://eveteran.gov.ua/benefits/pozachergove-bezoplatne-zabezpechennya-avtomobilem-zamina-na-novij">Посилання</a>
+
+<b>Першочергове безоплатне забезпечення автомобілем на термін експлуатації до десяти років</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergove-bezoplatne-zabezpechennya-avtomobilem-na-termin-ekspluataciyi-do-desyati-rokiv">Посилання</a>
+
+<b>Першочергове безоплатне забезпечення автомобілем - заміна на новий</b>
+<a href="https://eveteran.gov.ua/benefits/pershochergove-bezoplatne-zabezpechennya-avtomobilem-zamina-na-novij">Посилання</a>
+
+<b>Безоплатне забезпечення автомобілем першочергово із числа позачерговиків</b>
+<a href="https://eveteran.gov.ua/benefits/bezoplatne-zabezpechennya-avtomobilem-pershochergovo-iz-chisla-pozachergovikiv">Посилання</a>
+
+<b>Щомісячна виплата грошової компенсації витрат на автомобільне паливо з розрахунку 50 літрів високооктанового бензину на місяць відповідно до діючих цін на паливо</b>
+<a href="https://eveteran.gov.ua/benefits/shchomisyachna-viplata-groshovoyi-kompensaciyi-vitrat-na-avtomobilne-palivo-z-rozrahunku-50-litriv-visokooktanovogo-benzinu-na-misyac-vidpovidno-do-diyuchih-cin-na-palivo">Посилання</a>
+`,
+};
